112e147759db89b0d70dad4f6f51ff9a0b24441f
Koen Rijpstra <k.rijpstra@gmail.com>
2015-06-14 21:28:27 +0200

    Honey, I shrunk the inlines

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 // including plugins
 var gulp = require('gulp')
-    ,minifyHtml = require("gulp-minify-html");
+    ,minifyHtml = require("gulp-minify-html")
+    ,minifyInline = require('gulp-minify-inline');
 
 // task
 gulp.task('minify-html', function () {
     gulp.src('./*.html') // path to your files
         .pipe(minifyHtml({collapseWhitespace: true}))
+        .pipe(minifyInline())
         .pipe(gulp.dest('.'));
 });
 

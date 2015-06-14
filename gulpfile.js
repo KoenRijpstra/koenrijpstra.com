88f321732fa38850f0ea969c4042194671f6f252
Koen Rijpstra <k.rijpstra@gmail.com>
2015-06-14 21:02:56 +0200

    Added gulp to the mix

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// including plugins
+var gulp = require('gulp')
+    ,minifyHtml = require("gulp-minify-html");
+
+// task
+gulp.task('minify-html', function () {
+    gulp.src('./*.html') // path to your files
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('.'));
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('build', ['minify-html']);
--- shipitfile.js
+++ shipitfile.js
@@ -16,6 +16,24 @@ module.exports = function (shipit) {
         }
     });
 
+    shipit.on('fetched', function () {
+        shipit.start('install', 'build');
+    });
+
+    shipit.blTask('install', function () {
+        return shipit.local(
+            'npm install',
+            {cwd: shipit.config.workspace}
+        );
+    });
+
+    shipit.blTask('build', function () {
+        return shipit.local(
+            './node_modules/.bin/gulp build',
+            {cwd: shipit.config.workspace}
+        );
+    });
+
     // Serverpilot configuration support - Symlink current <- public
     shipit.on('published', function () {
         shipit.remote('cd ' + shipit.config.deployTo + ' && ln -nfs current public')
